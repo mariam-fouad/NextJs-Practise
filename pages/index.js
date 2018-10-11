c91bc70ca95000cea0975ec2349303fc6f203438
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-11 23:59:20 +0300

    add a jsx code for the root page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const indexPage = ()=>{
+    return (
+        <React.Fragment>
+            <h1>This is the main page</h1>
+            <p>rendered by Next js</p>
+        </React.Fragment>
+        
+    );
+};
+
+export default indexPage;
