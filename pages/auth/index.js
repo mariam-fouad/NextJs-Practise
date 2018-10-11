104c2a17d0abf6d9ca156bf4552ccecc04914904
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-12 00:00:05 +0300

    add a jsx code for the auth page

---
+++ pages/auth/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const indexAuthPage = ()=>{
+    return (
+        <React.Fragment>
+            <h1>This is the auth main page</h1>
+            <p>rendered by Next js</p>
+        </React.Fragment>
+        
+    );
+};
+
+export default indexAuthPage;
